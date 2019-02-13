68203d34c998e63e76800ad28a6ad7a111dd87e3
Alexandru Cristian GEORGESCU <sbucci@sqli.com>
2019-02-13 16:56:12 +0100

    profuct class

--- RestApi/Entities/Product.cs
+++ RestApi/Entities/Product.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RestApi.Entities
+﻿namespace RestApi.Entities
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
     }
 }
